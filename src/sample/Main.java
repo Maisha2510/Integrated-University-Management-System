44b9ec2c2041a2c90d69c446abfa11ec7ae16198
Maisha2510 <maisha2510@gmail.com>
2021-02-10 17:37:11 +0600

    userprofile updated by Maisha

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("DASHBOARD.fxml"));
-        primaryStage.setTitle("IUMS clone");
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("IUMS (Software Development Project 2.1");
         primaryStage.setScene(new Scene(root, 1500, 800));
         primaryStage.show();
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
+
     }
+
+
 }
